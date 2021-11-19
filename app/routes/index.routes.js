ed1ce9877b2d525fa7e0dd78d7501e319652c508
Josue Perez <josueperezf@gmail.com>
2021-11-19 15:43:13 -0300

    remover la seguridad para ingresar sin token

--- app/routes/index.routes.js
+++ app/routes/index.routes.js
@@ -2,14 +2,14 @@ const { validarJWT} = require('../middlewares');
 
 const rutas = [
     {path: '/api/auth',         route: require('./auth.routes'),        middlewareGlobals: [    ]},
-    {path: '/api/autores',      route: require('./autores.routes'),     middlewareGlobals: [validarJWT]},
-    {path: '/api/copias',       route: require('./copias.routes'),      middlewareGlobals: [validarJWT]},
-    {path: '/api/ediciones',    route: require('./ediciones.routes'),   middlewareGlobals: [validarJWT]},
-    {path: '/api/libros',       route: require('./libros.routes'),      middlewareGlobals: [validarJWT]},
-    {path: '/api/paises',       route: require('./pais.routes'),        middlewareGlobals: [validarJWT]},
-    {path: '/api/personas',     route: require('./personas.routes'),    middlewareGlobals: [validarJWT]},
-    {path: '/api/prestamos',    route: require('./prestamos.routes'),   middlewareGlobals: [validarJWT]},
-    {path: '/api/usuarios',     route: require('./usuarios.routes'),    middlewareGlobals: [validarJWT]},
+    {path: '/api/autores',      route: require('./autores.routes'),     middlewareGlobals: [    ]},
+    {path: '/api/copias',       route: require('./copias.routes'),      middlewareGlobals: [    ]},
+    {path: '/api/ediciones',    route: require('./ediciones.routes'),   middlewareGlobals: [    ]},
+    {path: '/api/libros',       route: require('./libros.routes'),      middlewareGlobals: [    ]},
+    {path: '/api/paises',       route: require('./pais.routes'),        middlewareGlobals: [    ]},
+    {path: '/api/personas',     route: require('./personas.routes'),    middlewareGlobals: [    ]},
+    {path: '/api/prestamos',    route: require('./prestamos.routes'),   middlewareGlobals: [    ]},
+    {path: '/api/usuarios',     route: require('./usuarios.routes'),    middlewareGlobals: [    ]},
 ];
 
 
